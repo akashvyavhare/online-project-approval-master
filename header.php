411402e54f40ef7cfc9e6982fbded0d0eccf9c20
akashvyavhare <91061457+akashvyavhare@users.noreply.github.com>
2022-05-31 00:58:47 +0530

    Revert "introduce dotenv, add header and footer"

--- header.php
+++ header.php
@@ -1,7 +1,4 @@
 <?php
-
-    require_once 'index.php';
-
     if(isset($_SESSION['role'])){
         switch($_SESSION['role']){
             case 'student':{
@@ -21,13 +18,3 @@
         }
     }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
-
-    require_once 'config.php';
-
-    global $dbConnection;
-
-    $dbConnection = Connection::getConnection();
-
     // session_start();
     // session_destroy();
-    // header("location:./login/student_login.php");
+    header("location:./login/student_login.php");
 
 ?>
