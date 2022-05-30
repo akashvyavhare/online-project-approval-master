ba31a7a935ec866b2079c037fc8403ad02d5530b
Uday Kokitkar <61972002+uday-kokitkar@users.noreply.github.com>
2022-05-30 15:54:05 +0530

    introduce dotenv, add header and footer

--- header.php
+++ header.php
@@ -1,4 +1,7 @@
 <?php
+
+    require_once 'index.php';
+
     if(isset($_SESSION['role'])){
         switch($_SESSION['role']){
             case 'student':{
@@ -18,3 +21,13 @@
         }
     }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
--- login/student_login.php
+++ login/student_login.php
@@ -1,19 +1,7 @@
 <?php
     session_start();
-    require_once('../server/connection.php');
     require_once("../header.php");
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
     <div class="container container-fluid">
         <div class="jumbotron">
             <h1 class="center">STUDENT Login</h1>
@@ -69,5 +57,6 @@
             }
         ?>
     </div>
-</body>
-</html>
+<?php
+require_once '../header.php';
+?>
